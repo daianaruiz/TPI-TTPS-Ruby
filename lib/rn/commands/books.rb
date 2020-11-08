e5f7c30959de562de94d94ca079fa455f5dc6ec3
Daiana Ruiz <daiana.ruiz.1996@gmail.com>
2020-11-08 20:55:15 -0300

    Implementacion de books.rb

--- lib/rn/commands/books.rb
+++ lib/rn/commands/books.rb
@@ -12,7 +12,17 @@ module RN
         ]
 
         def call(name:, **)
+          if Dir.exist?("#{Dir.home}/.my_rns/#{name}/")
+            puts "No se puede crear el cuaderno porque el nombre ya existe"
+          else
+            if name['/'] or name['\\']
+              puts "El nuevo nombre tiene caracteres invalidos -> / \\"
+            else
+              Dir.mkdir("#{Dir.home}/.my_rns/#{name}/")       
+            end
+          end
           warn "TODO: Implementar creación del cuaderno de notas con nombre '#{name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          
         end
       end
 
@@ -30,6 +40,16 @@ module RN
 
         def call(name: nil, **options)
           global = options[:global]
+          name = "global" if global
+            if Dir.exists?("#{Dir.home}/.my_rns/#{name}/")
+              if not Dir.empty?("#{Dir.home}/.my_rns/#{name}/")
+                Dir.each_child("#{Dir.home}/.my_rns/#{name}/") {|f| File.delete("#{Dir.home}/.my_rns/#{name}/#{f}")}  
+              end
+              Dir.rmdir("#{Dir.home}/.my_rns/#{name}/")
+            else
+              puts "El directorio no existe"
+            end
+
           warn "TODO: Implementar borrado del cuaderno de notas con nombre '#{name}' (global=#{global}).\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
         end
       end
@@ -42,6 +62,7 @@ module RN
         ]
 
         def call(*)
+          Dir.each_child("#{Dir.home}/.my_rns/") {|f| puts f}
           warn "TODO: Implementar listado de los cuadernos de notas.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
         end
       end
@@ -59,6 +80,15 @@ module RN
         ]
 
         def call(old_name:, new_name:, **)
+          if Dir.exists?("#{Dir.home}/.my_rns/#{old_name}/")
+            if new_name['/'] or new_name['\\']
+              puts "El nuevo nombre tiene caracteres invalidos -> / \\"
+            else
+              File.rename("#{Dir.home}/.my_rns/#{old_name}", "#{Dir.home}/.my_rns/#{new_name}")        
+            end    
+          else
+            puts "El directorio no existe"
+          end
           warn "TODO: Implementar renombrado del cuaderno de notas con nombre '#{old_name}' para que pase a llamarse '#{new_name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
         end
       end
